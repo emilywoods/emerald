4d6accad63e933a788064ea8685f71e95dce0201
Emily Woods <emilymonica.woods@gmail.com>
2017-02-19 19:06:03 +0000

    Class for numbers and include parsing of numbers

---
+++ spec/emerald/number_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+require "emerald/number"
+
+RSpec.describe Emerald::Number do
+  
+  describe "#==" do
+    it "can be equal" do
+      num1 = Emerald::Number.new(1)
+      num2 = Emerald::Number.new(1)
+      expect(num1).to eq(num2)
+    end
+
+    it "can be unequal with the same class" do
+      num1 = Emerald::Number.new(1)
+      num2 = Emerald::Number.new(2)
+      expect(num1).not_to eq(num2)
+    end
+
+    it "can be unequal with a different class" do
+      num1 = Emerald::Number.new(1)
+      num2 = Emerald::Atom.new(1)
+      expect(num1).not_to eq(num2)
+    end
+  end
+end
