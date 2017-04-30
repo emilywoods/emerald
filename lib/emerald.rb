5ad574952d13cb4c4662200c751242513296fc1d
Emily Woods <emilymonica.woods@gmail.com>
2017-05-01 00:18:32 +0100

    Write compiled code to file and evaluate

--- lib/emerald.rb
+++ lib/emerald.rb
@@ -1,5 +1,5 @@
 require_relative 'emerald/parser.rb'
-require_relative 'emerald/rubify'
+require_relative 'rubify'
 require_relative 'invalid-file-error'
 
 module Emerald
@@ -8,15 +8,13 @@ module Emerald
       @source = source
     end
 
-      # Read
-      pattern = /[a-zA-Z0-9\-_]+.lisp/
-      pattern.match(ARGV[0]) ? input_file = ARGV[0] : (raise InvalidFileTypeError)
-      read_file = File.read(input_file)
-      parsed_lisp = Parser.new(read_file).parse
-      compiled_lisp = Rubify.new(parsed_lisp).rubify
-      ruby_output = Evaluator.new(compiled_lisp).evaluate
-      # ruby_program = File.write(compiled_lisp)#write to ruby file?
-      # run(ruby_program) #Run ruby file
+    pattern = /[a-zA-Z0-9\-_]+.lisp/
+    pattern.match(ARGV[0]) ? input_file = ARGV[0] : (raise InvalidFileTypeError)
+    read_file = File.read(input_file)
+    parsed_lisp = Parser.new(read_file).parse
+    compiled_lisp = Rubify.new(parsed_lisp).rubify
 
+    File.open("compiled_lisp.rb", 'w') {|f| f.puts(compiled_lisp)}
+    puts eval File.read('compiled_lisp.rb')
   end
 end
