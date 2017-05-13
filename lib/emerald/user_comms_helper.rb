62eee09ee8f21d9d388eab0faa534541d5e41407
Emily Woods <emilymonica.woods@gmail.com>
2017-05-13 17:05:07 +0100

    Rubocop on emerald_spec

--- lib/emerald/user_comms_helper.rb
+++ lib/emerald/user_comms_helper.rb
@@ -1,7 +1,7 @@
 module Emerald
   class UserCommsHelper
 
-    ERROR_INCORRECT_FILE_TYPE = "Oops! The compiler requires an alphanumeric file without special characters"
+    ERROR_INVALID_FILE = "Oops! The compiler requires an alphanumeric file without special characters"
     ERROR_INITIALISE_WITH_STRING_IO = "Error: Initialize with StringIO objects"
 
     def initialize(stdout)
@@ -11,14 +11,14 @@ module Emerald
 
     def verify_input(input_file)
       pattern = /^[a-zA-Z0-9\-_.\/]+$/
-      !(input_file =~ pattern).nil? ? input_file : ( raise InvalidFileTypeError, ERROR_INCORRECT_FILE_TYPE )
+      !(input_file =~ pattern).nil? ? input_file : ( raise InvalidFileError, ERROR_INVALID_FILE )
     end
 
     def output_to_console(to_output)
       @stdout.puts(to_output.to_s)
     end
 
-    class InvalidFileTypeError < StandardError
+    class InvalidFileError < StandardError
     end
 
     class InvalidArgumentError < StandardError
