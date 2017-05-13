f1ea19c424b3603ff0cffe6d8919b9f5efbbc35a
Emily Woods <emilymonica.woods@gmail.com>
2017-05-13 17:05:07 +0100

    Add Rubocop to ensure consistent Ruby styling

--- spec/emerald/compiler_spec.rb
+++ spec/emerald/compiler_spec.rb
@@ -3,19 +3,19 @@ require_relative '../../lib/emerald/compiler'
 
 RSpec.describe 'Compiler' do
 
-    context "when the compiler receives a lisp input" do
-      it "parses the lisp and then returns a ruby string" do
-        lisp_file_contents = '(+ 2 2)'
-        compiled_lisp = Emerald::Compiler.new(lisp_file_contents).compile
-        expect(compiled_lisp).to eq('2.0 + 2.0')
-      end
+  context 'when the compiler receives a lisp input' do
+    it 'parses the lisp and then returns a ruby string' do
+      lisp_file_contents = '(+ 2 2)'
+      compiled_lisp = Emerald::Compiler.new(lisp_file_contents).compile
+      expect(compiled_lisp).to eq('2.0 + 2.0')
     end
+  end
 
-    context "when the contents of the file are empty" do
-      it "parses the lisp and then returns an empty string" do
-        lisp_file_contents = ''
-        compiled_lisp = Emerald::Compiler.new(lisp_file_contents).compile
-        expect(compiled_lisp).to eq('')
-      end
+  context 'when the contents of the file are empty' do
+    it 'parses the lisp and then returns an empty string' do
+      lisp_file_contents = ''
+      compiled_lisp = Emerald::Compiler.new(lisp_file_contents).compile
+      expect(compiled_lisp).to eq('')
     end
+  end
 end
--- spec/emerald_spec.rb
+++ spec/emerald_spec.rb
@@ -3,91 +3,94 @@ require 'tempfile'
 require 'emerald/user_comms_helper'
 require 'emerald/parser'
 require 'emerald/compiler'
-include Emerald
 
 RSpec.describe 'Emerald' do
 
-  let(:stdout) {spy('STDOUT')}
-  let(:user_comms) {Emerald::UserCommsHelper.new(stdout)}
+  let(:stdout) { spy('STDOUT') }
+  let(:user_comms) { Emerald::UserCommsHelper.new(stdout) }
+  let(:parser) { Emerald::Parser }
 
-
-  describe "given a non-lisp file is passed in" do
-    context "an invalid file name is passed" do
-      it "returns an InvalidFileType error" do
-        expect {user_comms.verify_input('test$-!f&ile')}.to \
-          raise_error(Emerald::UserCommsHelper::InvalidFileTypeError).with_message(Emerald::UserCommsHelper::ERROR_INCORRECT_FILE_TYPE)
-      end
+  describe 'given an invalid file is passed in' do
+    it 'raises an InvalidFileType error' do
+      expect { user_comms.verify_input('test$-!f&ile') }.to\
+        raise_error(Emerald::UserCommsHelper::InvalidFileError)
     end
 
-    context "when no file is passed" do
-      it "returns an InvalidFileType error" do
-        expect {user_comms.verify_input('')}.to \
-        raise_error(Emerald::UserCommsHelper::InvalidFileTypeError).with_message(Emerald::UserCommsHelper::ERROR_INCORRECT_FILE_TYPE)
-      end
+    it 'raises an error with invalid file message' do
+      expect { user_comms.verify_input('test$-!f&ile') }.to\
+        raise_error.with_message(Emerald::UserCommsHelper::ERROR_INVALID_FILE)
     end
   end
 
-  describe "given a valid lisp file is passed" do
-    let(:test_file) {Tempfile.new('new-file')}
+  describe 'given no file is passed in' do
+    it 'returns an InvalidFileType error' do
+      expect { user_comms.verify_input('') }.to\
+        raise_error(Emerald::UserCommsHelper::InvalidFileError)
+    end
 
-    it "raises an InvalidListError when it receives an invalid list without a closing bracket" do
+    it 'raises an error with incorrect file type message' do
+      expect { user_comms.verify_input('') }.to \
+        raise_error.with_message(Emerald::UserCommsHelper::ERROR_INVALID_FILE)
+    end
+  end
+
+  describe 'given a valid lisp file is passed' do
+    let(:test_file) { Tempfile.new('new-file') }
+
+    it 'raises an InvalidListError for a list without a closing bracket' do
       create_test_file('(+ 5 2')
 
-      lisp_file_contents = File.read(test_file)
-      compiler = Emerald::Compiler.new(lisp_file_contents)
-
-      expect {compiler.compile}.to raise_error(Emerald::Parser::InvalidListError).with_message(Emerald::Parser::ERROR_INVALID_LIST)
+      expect { compile_lisp(test_file) }.to \
+        raise_error(parser::InvalidListError)
+      expect { compile_lisp(test_file) }.to \
+        raise_error.with_message(parser::ERROR_INVALID_LIST)
     end
 
-    it "raises an InvalidListError when it receives an invalid list without an opening bracket" do
+    it 'raises an InvalidListError for a list without an opening bracket' do
       create_test_file('+ 5 2 )')
 
-      lisp_file_contents = File.read(test_file)
-      compiler = Emerald::Compiler.new(lisp_file_contents)
-
-      expect {compiler.compile}.to raise_error(Emerald::Parser::InvalidListError).with_message(Emerald::Parser::ERROR_INVALID_LIST)
+      expect { compile_lisp(test_file) }.to \
+        raise_error(parser::InvalidListError)
+      expect { compile_lisp(test_file) }.to \
+        raise_error.with_message(parser::ERROR_INVALID_LIST)
     end
 
-
-    it "raises an InvalidListError when it receives an invalid lisp function expression" do
+    it 'raises an InvalidListError for an invalid lisp expression' do
       create_test_file('( 5 + 5 )')
 
-      lisp_file_contents = File.read(test_file)
-      compiler = Emerald::Compiler.new(lisp_file_contents)
-
-      expect {compiler.compile}.to raise_error(Emerald::Rubify::InvalidLispFunctionError)
+      expect { compile_lisp(test_file) }.to \
+        raise_error(Emerald::Rubify::InvalidLispFunctionError)
     end
 
-
-    it "compiles and evaluates the lisp when it receives a valid lisp expression with a single number" do
+    it 'compiles and evaluates a valid lisp expression with a number' do
       create_test_file('5')
 
       stdout = `ruby lib/emerald.rb "#{test_file.path}"`.chomp
       expect(stdout).to eq('5.0')
     end
 
-    it "compiles and evaluates the lisp when it receives a valid lisp expression with an addition operation" do
+    it 'compiles and evaluates a valid lisp expression with addition' do
       create_test_file('(+ 2 5)')
 
       stdout = `ruby lib/emerald.rb "#{test_file.path}"`.chomp
       expect(stdout).to eq('7.0')
     end
 
-    it "compiles and evaluates the lisp when it receives a valid lisp expression with a subtraction operation" do
+    it 'compiles and evaluates a valid lip expression with subtraction' do
       create_test_file('(- 5 2 1)')
 
       stdout = `ruby lib/emerald.rb "#{test_file.path}"`.chomp
       expect(stdout).to eq('2.0')
     end
 
-    it "compiles and evaluates the lisp when it receives a valid lisp expression with a division operation" do
+    it 'compiles and evaluates a valid lip expression with division' do
       create_test_file('(/ 20 2 2)')
 
       stdout = `ruby lib/emerald.rb "#{test_file.path}"`.chomp
       expect(stdout).to eq('5.0')
     end
 
-    it "compiles and evaluates the lisp when it receives a valid lisp expression with a multiplication operation" do
+    it 'compiles and evaluates a valid lip expression with multiplication' do
       create_test_file('(* 12 2 2)')
 
       stdout = `ruby lib/emerald.rb "#{test_file.path}"`.chomp
@@ -99,9 +102,14 @@ RSpec.describe 'Emerald' do
       test_file.close
     end
 
+    def compile_lisp(test_file)
+      lisp_file_contents = File.read(test_file)
+      compiler = Emerald::Compiler.new(lisp_file_contents)
+      compiler.compile
+    end
+
     after(:each) do
       test_file.unlink
     end
-
   end
 end
